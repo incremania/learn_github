c3130a388deb5e7f9b613eb851cca7caf6bbe419
okiki <companychris00@gmail.com>
2022-08-22 18:14:51 +0100

    still working

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
 const toggleBtn = document.querySelector('.nav-toggle-btn')
-const navContainer = document.querySelector('.nav-container-div')
-const navbar = document.querySelector('.navbar')
-const links = document.querySelector('.nav-items')
-const p = document.querySelector('p')
+const sideBar = document.querySelector('.side-bar')
+const closeBtn = document.querySelector('.side-close-btn')
 
 toggleBtn.addEventListener('click', function() {
-    const containerHeight = navContainer.getBoundingClientRect().height
-    const linksHeight = links.getBoundingClientRect().height
-    
-    // navContainer.classList.add('show-nav')
-    if(containerHeight === 0) {
-        navContainer.style.height = `${linksHeight + 20}px`
-    } else {
-        navContainer.style.height = 0
-    }
-   
-
+    sideBar.classList.add('show-nav')
     
 })
 
-window.addEventListener('scroll', function() {
-    const navHeight = navbar.getBoundingClientRect().height
-    
-    const windowUsedHeight = window.pageYOffset
-    if(windowUsedHeight > navHeight) {
-        p.style.marginTop = '110px'
-      navbar.classList.add('fixed-nav')
-     
-    } else {
-        navbar.classList.remove('fixed-nav')
-        p.style.marginTop = '10px'
-        
-    }
-
+closeBtn.addEventListener('click', function() {
+    sideBar.classList.remove('show-nav')
 })
+
+
