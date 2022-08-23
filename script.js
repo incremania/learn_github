11f091d49203302c9ff4cdaf3ddc533c594ed0ab
okiki <companychris00@gmail.com>
2022-08-23 18:58:53 +0100

    create img

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const toggleBtn = document.querySelector('.nav-toggle-btn')
 const sideBar = document.querySelector('.side-bar')
 const closeBtn = document.querySelector('.side-close-btn')
+const contactBtn = document.querySelector('.contact-support')
+const chatSupport = document.querySelector('.chat-support')
+const chats = document.querySelectorAll('chat')
 
 toggleBtn.addEventListener('click', function() {
     sideBar.classList.add('show-nav')
@@ -10,5 +13,18 @@ toggleBtn.addEventListener('click', function() {
 closeBtn.addEventListener('click', function() {
     sideBar.classList.remove('show-nav')
 })
+
+
+contactBtn.addEventListener('click', function(e) {
+    e.preventDefault()
+    chatSupport.classList.toggle('hide-link')
+})
+
+chats.forEach(chat => {
+    chat.addEventListener('click', function(e) {
+        e.preventDefault()
+    })
+})
+
 
 
