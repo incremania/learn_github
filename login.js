88a5943272469fb03f55aac306c62e3ccf347105
okiki <companychris00@gmail.com>
2022-08-26 19:28:23 +0100

    Update login.js

---
+++ login.js
@@ -0,0 +1,8 @@
+// if(!firstName.value.match(/^[A-Za-z\s]*$/ ) ) {
+//     errorFirstName.innerHTML = 'enter a valid name without any characters'
+//     errorFirstName.classList.add('notvalid')
+// }
+
+// axios.get('https://reqres.in/api/users?page=2')
+// .then(res => console.log(res))
+// .catch(err => console.log(err))
