1aa0bf4826da7bd3c791922dacc23d7fd120ec6b
okiki <companychris00@gmail.com>
2022-08-29 16:28:25 +0100

    IMAGE CHANGED

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-// const toggleBtn = document.querySelector('.nav-toggle-btn')
-// const sideBar = document.querySelector('.side-bar')
-// const closeBtn = document.querySelector('.side-close-btn')
-// const contactBtn = document.querySelector('.contact-support')
-// const chatSupport = document.querySelector('.chat-support')
-// const chats = document.querySelectorAll('chat')
-// const navLinks = document.querySelectorAll('.nav-link')
-
-// toggleBtn.addEventListener('click', function() {
-//     sideBar.classList.add('show-nav') 
-// })
-
-// closeBtn.addEventListener('click', function() {
-//     sideBar.classList.remove('show-nav')
-// })
-
-
-// contactBtn.addEventListener('click', function(e) {
-//     e.preventDefault()
-//     chatSupport.classList.remove('hide-link') 
-// })
-
-// chats.forEach(chat => {
-//     chat.addEventListener('click', function(e) {
-//         e.preventDefault()
-//     })
-// })
-
-// navLinks.forEach(navLink => {
-//     navLink.addEventListener('click', function() {
-//         sideBar.classList.remove('show-nav')
-//     })
-// })
-
 
 const toggleBtn = document.querySelector('.fa-bars');
 const sidebar = document.querySelector('.nav-bottom')
 const sidebarloseBtn = document.querySelector('.fa-times')
+const contactBtn = document.querySelector('.contact-support')
+const chatSupport = document.querySelector('.chat-support')
+// const chats = document.querySelectorAll('.chat')
+const navLinks = document.querySelectorAll('.item-link')
+
+contactBtn.addEventListener('click', function(e) {
+    e.preventDefault()
+    chatSupport.classList.remove('hide-link') 
+})
+
+// chats.forEach(chat => {
+//     chat.addEventListener('click', function(e) {
+//         // e.preventDefault()
+//     })
+// })
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', function() {
+        sidebar.classList.remove('show-sidebar')
+    })
+})
+
+
 toggleBtn.addEventListener('click', function() {
   sidebar.classList.toggle('show-sidebar')
 })
